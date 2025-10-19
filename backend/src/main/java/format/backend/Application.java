d6cfd9055f304cc7e151b54f1ba17c75e2af2714
Paw565pl <paw565pl@gmail.com>
2025-10-19 15:43:21 +0200

    chore: add config annotations

--- backend/src/main/java/format/backend/Application.java
+++ backend/src/main/java/format/backend/Application.java
@@ -1,9 +1,17 @@
 package format.backend;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableMongoAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class Application {
 
     public static void main(String[] args) {
