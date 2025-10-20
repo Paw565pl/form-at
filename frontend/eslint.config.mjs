a32bc43c24ce637d5235958814a0e395e6479352
Paw565pl <paw565pl@gmail.com>
2025-10-20 15:10:07 +0200

    chore: setup eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// @ts-check
+
+import { includeIgnoreFile } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
+import { defineConfig } from "eslint/config";
+import { fileURLToPath } from "node:url";
+import { dirname } from "path";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,17 +16,23 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
-export default eslintConfig;
+export default defineConfig(
+  includeIgnoreFile(gitignorePath),
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  ...compat.extends("next/core-web-vitals"),
+  {
+    plugins: {
+      "no-relative-import-paths": noRelativeImportPaths,
+    },
+    rules: {
+      "no-relative-import-paths/no-relative-import-paths": [
+        "error",
+        { rootDir: "src", prefix: "@", allowSameFolder: false },
+      ],
+    },
+  }
+);
