f73e12a7f31a805d4961e518f55dd6ddd8115e09
Paw565pl <paw565pl@gmail.com>
2025-10-20 15:42:00 +0200

    chore: setup next themes

--- frontend/src/core/layouts/root-layout.tsx
+++ frontend/src/core/layouts/root-layout.tsx
@@ -1,9 +1,12 @@
+import { Providers } from "@/core/providers/providers";
 import { PropsWithChildren } from "react";
 
 export const RootLayout = ({ children }: PropsWithChildren) => {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 };
