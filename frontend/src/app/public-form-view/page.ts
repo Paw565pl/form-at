4d15628384bf6f174734646e45f90553af1f871f
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2025-10-25 10:57:04 +0200

    chore(public-form-view): create basic public form layout

---
+++ frontend/src/app/public-form-view/page.ts
@@ -0,0 +1,3 @@
+import PublicFormView from "@/features/public-form-view";
+
+export default PublicFormView;
---
+++ frontend/src/features/public-form-view/index.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/core/components/ui/button";
+import { HatGlasses, ListChecksIcon, MoreHorizontal } from "lucide-react";
+import Image from "next/image";
+
+export default function PublicFormView() {
+  return (
+    <div className="m-auto flex max-w-5xl flex-col">
+      <div className="relative mb-[25px] flex h-[200px] w-full max-w-5xl items-end">
+        <Image
+          src="/banner.jpg"
+          alt="Background"
+          fill
+          style={{ objectFit: "cover" }}
+          priority
+          className="-z-10 rounded-3xl"
+        />
+        <Button size={"icon-sm"} className="absolute top-4 right-4">
+          <MoreHorizontal />
+        </Button>
+        <div className="absolute bottom-[-25px] left-4 h-[100px] w-[100px] rounded-full border-2 border-black bg-red-500"></div>
+        <div className="absolute right-4 bottom-4 flex gap-2">
+          <Button size={"sm"}>
+            <ListChecksIcon />
+            Wykonaj Quiz
+          </Button>
+          <Button size={"sm"}>
+            <HatGlasses />
+            Wykonaj Anonimowo
+          </Button>
+        </div>
+      </div>
+      <div className="my-2 flex justify-between">
+        <h1 className="text-4xl font-extrabold">Quiz o kotach</h1>
+        <div className="inline-flex h-9 w-min items-center justify-center rounded-xl border-2 border-gray-600 bg-blue-500 px-4 py-2 whitespace-nowrap text-white">
+          Quiz wykonany: 20.12.2025
+        </div>
+      </div>
+      <div className="flex gap-6 text-gray-500">
+        <h2>12 pytań</h2>
+        <h2>1020 odpowiedzi</h2>
+        <h2>Szacunkowy czas wykonania: 5 minut</h2>
+      </div>
+      <p>
+        Fajniutki quiz o kotach z dosyć dłuższym opisem Fajniutki quiz o kotach
+        z dosyć dłuższym opisem Fajniutki quiz o kotach z dosyć dłuższym opisem
+        Fajniutki quiz o kotach z dosyć dłuższym opisem Fajniutki quiz o kotach
+        z dosyć dłuższym opisem Fajniutki quiz o kotach z dosyć dłuższym opisem
+        Fajniutki quiz o kotach z dosyć dłuższym opisem Fajniutki quiz o kotach
+        z dosyć dłuższym opisem Fajniutki quiz o kotach z dosyć dłuższym opisem
+        Fajniutki quiz o kotach z dosyć dłuższym opisem Fajniutki quiz o kotach
+        z dosyć dłuższym opisem Fajniutki quiz o kotach z dosyć dłuższym opisem
+      </p>
+      <p className="w-full text-right text-gray-500">20.10.2025</p>
+    </div>
+  );
+}
