9b9e941400336edec61dcc57537c6d4a32f78a1b
Paw565pl <paw565pl@gmail.com>
2025-10-28 09:36:32 +0100

    chore: set session refetch interval

--- frontend/src/core/providers/server-providers.tsx
+++ frontend/src/core/providers/server-providers.tsx
@@ -4,11 +4,13 @@ import { NextIntlClientProvider } from "next-intl";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { PropsWithChildren } from "react";
 
+const sessionRefetchInterval = 60 * 30; // 30 minutes in seconds
+
 export const ServerProviders = async ({ children }: PropsWithChildren) => {
   const session = await auth();
 
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={session} refetchInterval={sessionRefetchInterval}>
       <NextIntlClientProvider>
         <NuqsAdapter>{children}</NuqsAdapter>
       </NextIntlClientProvider>
