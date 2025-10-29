dc8d647fbf7ae971bb85bf341db1f12cd1f578dc
Mateusz Domino <mdomino60@gmail.com>
2025-10-29 19:43:47 +0100

    FEAT: add comment entity

---
+++ backend/src/main/java/format/backend/comment/entity/CommentEntity.java
@@ -0,0 +1,37 @@
+package format.backend.comment.entity;
+
+import format.backend.auth.entity.UserEntity;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.*;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "comments")
+public class CommentEntity {
+
+    @MongoId(targetType = FieldType.OBJECT_ID)
+    @Field(name = "_id")
+    private String id;
+
+    @DocumentReference(lazy = true)
+    @Field(name = "author")
+    private UserEntity author;
+
+    @NonNull
+    @Field(name = "content")
+    private String content;
+
+    @CreatedDate
+    @Field("createdAt")
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Field("updatedAt")
+    private Instant updatedAt;
+}
