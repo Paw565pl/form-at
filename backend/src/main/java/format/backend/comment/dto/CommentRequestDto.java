84f054bf2d9b5cd472f711db6165ed29d1503bb4
Mateusz Domino <mdomino60@gmail.com>
2025-10-30 23:19:13 +0100

    FIX: apply further spotless changes

--- backend/src/main/java/format/backend/comment/dto/CommentRequestDto.java
+++ backend/src/main/java/format/backend/comment/dto/CommentRequestDto.java
@@ -12,6 +12,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class CommentRequestDto {
-    @NotBlank(message = "Comment content cannot be blank") @Size(min = 3, max = 500, message = "Comment must be between 3 and 500 characters long")
-    private String content;
+    @NotBlank(message = "Comment content cannot be blank") @Size(min = 3, max = 500, message = "Comment must be between 3 and 500 characters long") private String content;
 }
