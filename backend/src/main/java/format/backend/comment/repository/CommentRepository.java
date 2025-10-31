6a1f18c7769c120f8f4ba795320ce10ce2994c01
Mateusz Domino <mdomino60@gmail.com>
2025-10-31 21:17:54 +0100

    FEAT: add comment repository

---
+++ backend/src/main/java/format/backend/comment/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package format.backend.comment.repository;
+
+import format.backend.comment.entity.CommentEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends MongoRepository<CommentEntity, String> {}
