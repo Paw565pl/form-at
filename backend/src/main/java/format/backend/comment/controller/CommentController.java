2c60fbb3d01d054f9e2a9263077baefd305c36e3
Mateusz Domino <mdomino60@gmail.com>
2025-10-31 23:40:40 +0100

    FEAT: add getter for all comments for form

--- backend/src/main/java/format/backend/comment/controller/CommentController.java
+++ backend/src/main/java/format/backend/comment/controller/CommentController.java
@@ -1,4 +1,32 @@
 package format.backend.comment.controller;
 
+import format.backend.comment.dto.CommentResponseDto;
+import format.backend.comment.service.CommentService;
+import format.backend.form.validator.ValidFormId;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/forms/{formId}/comments")
 public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping
+    public Page<CommentResponseDto> findAll(
+            @AuthenticationPrincipal Jwt jwt,
+            @NotBlank @ValidFormId @PathVariable String formId,
+            @PageableDefault(size = 10, sort = "createdAt", direction = DESC) Pageable pageable) {
+        return commentService.findAll(formId, pageable);
+    }
 }
--- backend/src/main/java/format/backend/comment/service/CommentService.java
+++ backend/src/main/java/format/backend/comment/service/CommentService.java
@@ -1,4 +1,38 @@
 package format.backend.comment.service;
 
+import format.backend.auth.repository.UserRepository;
+import format.backend.comment.dto.CommentResponseDto;
+import format.backend.comment.entity.CommentEntity;
+import format.backend.comment.mapper.CommentMapper;
+import format.backend.comment.repository.CommentRepository;
+import format.backend.form.repository.FormRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+    private final MongoTemplate mongoTemplate;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final FormRepository formRepository;
+    private final CommentMapper commentMapper;
+
+    public Page<CommentResponseDto> findAll(String formId, Pageable pageable) {
+        var form = formRepository.findById(formId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Form not found"));
+
+        Page<CommentEntity> comments = commentRepository.findByFormId(formId, pageable);
+        if (comments.isEmpty()) {
+            return Page.empty(pageable);
+        }
+
+        return comments.map(commentMapper::toResponseDto);
+    }
 }
