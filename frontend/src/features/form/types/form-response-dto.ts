9def8049e614563f96e9771c24a44792c5f31a78
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2025-11-03 12:42:27 +0100

    feature: create form-response-dto

---
+++ frontend/src/features/form/types/form-response-dto.ts
@@ -0,0 +1,42 @@
+import { QuestionResponseDto } from "./question-response-dto";
+
+export enum FormStatus {
+  Public = "PUBLIC",
+  Unpublic = "UNPUBLIC",
+  Private = "PRIVATE",
+  Draft = "DRAFT",
+  Closed = "CLOSED",
+}
+
+export enum Language {
+  En = "EN",
+  Pl = "PL",
+}
+
+export enum ShuffleVariant {
+  Null = "NULL",
+  Questions = "QUESTIONS",
+  Answers = "ANSWERS",
+  Both = "BOTH",
+}
+
+export interface FormResponseDto {
+  id: string;
+  name: string;
+  slug: string;
+  description?: string;
+  language: Language;
+  status: FormStatus;
+  shuffleVariant: ShuffleVariant;
+  thanksMessage: string;
+  estimatedDuration: number;
+  thumbnailKey: string;
+  allowsQuestionPreview: boolean;
+  allowsGuestSubmissions: boolean;
+  saveSubmissions: boolean;
+  authorId: string;
+  createdAt: string;
+  updatedAt: string;
+  submissionCount: number;
+  questions: QuestionResponseDto[];
+}
