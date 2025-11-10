e67472d6a585c6716e557ddf9e6c4a607e0c090b
Paw565pl <paw565pl@gmail.com>
2025-11-10 18:39:48 +0100

    chore: remove sub mapper

--- backend/src/main/java/format/backend/submission/mapper/SubmissionMapper.java
+++ backend/src/main/java/format/backend/submission/mapper/SubmissionMapper.java
@@ -8,13 +8,16 @@ import format.backend.submission.entity.SubmissionEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.springframework.lang.Nullable;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = SubmissionAnswerMapper.class)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface SubmissionMapper {
 
     SubmissionResponseDto toResponseDto(SubmissionEntity submissionEntity);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "form", source = "form")
+    @Mapping(target = "author", source = "author")
     @Mapping(target = "createdAt", ignore = true)
-    SubmissionEntity toEntity(SubmissionRequestDto submissionRequestDto, FormEntity form, UserEntity author);
+    SubmissionEntity toEntity(SubmissionRequestDto submissionRequestDto, FormEntity form, @Nullable UserEntity author);
 }
