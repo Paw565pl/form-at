7133fc7e4b6ea79af83b2f62f1a0cf7b869b6999
Paw565pl <paw565pl@gmail.com>
2025-11-10 15:38:56 +0100

    fix: add missing annotation

--- backend/src/main/java/format/backend/submission/dto/SubmissionRequestDto.java
+++ backend/src/main/java/format/backend/submission/dto/SubmissionRequestDto.java
@@ -1,8 +1,9 @@
 package format.backend.submission.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import org.springframework.lang.NonNull;
 
 public record SubmissionRequestDto(
-        @NotEmpty(message = "Answers cannot be empty") @NonNull List<SubmissionAnswerRequestDto> answers) {}
+        @NotEmpty(message = "Answers cannot be empty") @Valid @NonNull List<SubmissionAnswerRequestDto> answers) {}
