66f75ac7f2a3fcbe8af762d5fa1698133ce300fe
Mateusz Domino <mdomino60@gmail.com>
2025-11-10 21:17:50 +0100

    FIX: remove isEmpty from validator

--- backend/src/main/java/format/backend/core/validator/ObjectIdValidator.java
+++ backend/src/main/java/format/backend/core/validator/ObjectIdValidator.java
@@ -7,7 +7,7 @@ import org.bson.types.ObjectId;
 public class ObjectIdValidator implements ConstraintValidator<ValidObjectId, String> {
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        if (value == null || value.isEmpty()) return true;
+        if (value == null) return true;
 
         return ObjectId.isValid(value);
     }
