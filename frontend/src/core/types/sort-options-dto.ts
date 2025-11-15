1702c92ca4632b39e7d24a222a99db66a45c3c88
Paw565pl <paw565pl@gmail.com>
2025-11-15 11:40:09 +0100

    chore: create sort options dto

---
+++ frontend/src/core/types/sort-options-dto.ts
@@ -0,0 +1,13 @@
+class SortOptionsDto {
+  constructor(
+    public field: string,
+    public order: "asc" | "desc",
+  ) {
+    this.field = field;
+    this.order = order;
+  }
+
+  public getSearchParamValue(): string {
+    return `${this.field},${this.order}`;
+  }
+}
