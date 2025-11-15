93fa2c58be075d94f7e8ab0a270665a127275144
Paw565pl <paw565pl@gmail.com>
2025-11-15 13:06:00 +0100

    feat: create fetch form page hook

---
+++ frontend/src/features/form-list/hooks/use-fetch-form-page.ts
@@ -0,0 +1,60 @@
+import { apiService } from "@/core/services/api-service";
+import { ErrorResponseDto } from "@/core/types/error-response-dto";
+import { FormFilterOptionsDto } from "@/core/types/form-filter-options-dto";
+import { FormListResponseDto } from "@/core/types/form-list-response-dto";
+import { formSortOptions } from "@/core/types/form-sort-options";
+import { PaginatedResponseDto } from "@/core/types/paginated-response-dto";
+import {
+  infiniteQueryOptions,
+  QueryClient,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+const getFetchFormPageOptions = (
+  formFilterOptionsDto?: FormFilterOptionsDto,
+  formSortOptionKey?: keyof typeof formSortOptions,
+) => {
+  const formSortOptionsDto =
+    formSortOptionKey && formSortOptions[formSortOptionKey];
+
+  return infiniteQueryOptions<
+    PaginatedResponseDto<FormListResponseDto>,
+    AxiosError<ErrorResponseDto>
+  >({
+    queryKey: ["forms", formFilterOptionsDto, formSortOptionsDto],
+    queryFn: async ({ pageParam }) => {
+      const { data } = await apiService.get<
+        PaginatedResponseDto<FormListResponseDto>
+      >("/api/v1/forms", {
+        params: {
+          ...formFilterOptionsDto,
+          sort: formSortOptionsDto?.getSearchParamValue(),
+          page: pageParam,
+        },
+      });
+      return data;
+    },
+    initialPageParam: 0,
+    getNextPageParam: ({ page }) =>
+      page.number + 1 < page.totalPages ? page.number + 1 : undefined,
+    staleTime: 1000 * 60 * 10, // 10 minutes
+  });
+};
+
+export const useFetchFormPage = (
+  formFilterOptionsDto?: FormFilterOptionsDto,
+  formSortOptionsKey?: keyof typeof formSortOptions,
+) =>
+  useInfiniteQuery(
+    getFetchFormPageOptions(formFilterOptionsDto, formSortOptionsKey),
+  );
+
+export const prefetchFormPage = (
+  queryClient: QueryClient,
+  formFilterOptionsDto?: FormFilterOptionsDto,
+  formSortOptionsKey?: keyof typeof formSortOptions,
+) =>
+  queryClient.prefetchInfiniteQuery(
+    getFetchFormPageOptions(formFilterOptionsDto, formSortOptionsKey),
+  );
