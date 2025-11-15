bd70357849bb93b36ed3a9d666fcd8a863753e38
Paw565pl <paw565pl@gmail.com>
2025-11-15 11:57:33 +0100

    chore: create form sort options

---
+++ frontend/src/core/types/form-sort-options.ts
@@ -0,0 +1,13 @@
+export const formSortOptions = {
+  "estimatedDuration:asc": new SortOptionsDto("estimatedDuration", "asc"),
+  "estimatedDuration:desc": new SortOptionsDto("estimatedDuration", "desc"),
+
+  "submissionsCount:asc": new SortOptionsDto("submissionsCount", "asc"),
+  "submissionsCount:desc": new SortOptionsDto("submissionsCount", "desc"),
+
+  "createdAt:asc": new SortOptionsDto("createdAt", "asc"),
+  "createdAt:desc": new SortOptionsDto("createdAt", "desc"),
+
+  "updatedAt:asc": new SortOptionsDto("updatedAt", "asc"),
+  "updatedAt:desc": new SortOptionsDto("updatedAt", "desc"),
+} as const;
