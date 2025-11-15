caeb9709ca93696cf67c3ed6ff0f533b1e561d50
Paw565pl <paw565pl@gmail.com>
2025-11-15 13:11:16 +0100

    feat: create use fetch form detail query

---
+++ frontend/src/features/form-list/hooks/use-fetch-form-detail.ts
@@ -0,0 +1,25 @@
+import { apiService } from "@/core/services/api-service";
+import { ErrorResponseDto } from "@/core/types/error-response-dto";
+import { FormDetailResponseDto } from "@/core/types/form-detail-response-dto";
+import { QueryClient, queryOptions, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+const getFetchFormDetailQueryOptions = (idOrSlug: string) =>
+  queryOptions<FormDetailResponseDto, AxiosError<ErrorResponseDto>>({
+    queryKey: ["forms", idOrSlug],
+    queryFn: async () => {
+      const { data } = await apiService.get<FormDetailResponseDto>(
+        `/api/v1/forms/${idOrSlug}`,
+      );
+      return data;
+    },
+    staleTime: 1000 * 60 * 10, // 10 minutes
+  });
+
+export const useFetchFormDetail = (idOrSlug: string) =>
+  useQuery(getFetchFormDetailQueryOptions(idOrSlug));
+
+export const prefetchFormDetail = (
+  queryClient: QueryClient,
+  idOrSlug: string,
+) => queryClient.prefetchQuery(getFetchFormDetailQueryOptions(idOrSlug));
