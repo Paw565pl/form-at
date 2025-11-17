2e3bfaab65862ee1871402df72424a7c81d2d6d2
Mateusz Domino <mdomino60@gmail.com>
2025-11-17 19:30:57 +0100

    REFACTOR: add validated annotation

--- backend/src/main/java/format/backend/comment/controller/CommentController.java
+++ backend/src/main/java/format/backend/comment/controller/CommentController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/forms/{formIdOrSlug}/comments")
