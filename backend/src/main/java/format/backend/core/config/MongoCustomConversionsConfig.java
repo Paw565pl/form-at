1e0b69b65d80995687b2e46ea8519928e73748d7
Paw565pl <paw565pl@gmail.com>
2025-11-27 14:26:08 +0100

    feat: create language read converter

---
+++ backend/src/main/java/format/backend/core/config/MongoCustomConversionsConfig.java
@@ -0,0 +1,16 @@
+package format.backend.core.config;
+
+import format.backend.form.converter.LanguageReadConverter;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+@Configuration
+class MongoCustomConversionsConfig {
+
+    @Bean
+    MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(List.of(new LanguageReadConverter()));
+    }
+}
