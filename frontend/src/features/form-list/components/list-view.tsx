0d34f2d984e687a54134bf519a8680fc0feb0a3b
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2025-11-30 23:39:02 +0100

    FEAT: replace Card component with FormCard for improved form display

--- frontend/src/features/form-list/components/list-view.tsx
+++ frontend/src/features/form-list/components/list-view.tsx
@@ -1,42 +1,11 @@
-import { Card } from "@/core/components/ui/card";
-import { formatDuration } from "@/core/utils/formatDuration";
+import { FormCard } from "@/core/components/form-card/form-card";
 import { forms } from "@/features/form-list/example-forms";
-import { useFormatter, useTranslations } from "next-intl";
-import Link from "next/link";
 
 export const ListView = () => {
-  const t = useTranslations("formListPage");
-  const format = useFormatter();
-
   return (
     <div className="flex flex-col gap-2">
       {forms.map((form) => (
-        <Link href={{ pathname: `/forms/${form.id}` }} key={form.id}>
-          <Card
-            key={form.id}
-            className="hover:border-primary gap-1 p-3 transition-all"
-          >
-            <header className="flex flex-wrap items-center justify-between gap-1">
-              <h1 className="font-medium">{form.name}</h1>
-              <span className="text-muted-foreground text-sm">
-                {form.author
-                  ? `${t("by", { name: form.author.name })}`
-                  : t("byUnknown")}
-              </span>
-            </header>
-            <p className="text-sm lg:mr-36">{form.description}</p>
-            <footer className="text-muted-foreground mt-1 flex flex-wrap justify-between text-sm">
-              <span>
-                {t("questions", { count: form.questions.length })} •{" "}
-                {t("submissions", { count: form.submissionsCount })} •{" "}
-                {formatDuration(form.estimatedDuration)}
-              </span>
-              <span className="text-muted-foreground text-sm">
-                {format.dateTime(new Date(form.createdAt), "long")}
-              </span>
-            </footer>
-          </Card>
-        </Link>
+        <FormCard key={form.id} form={form} showAuthor />
       ))}
     </div>
   );
