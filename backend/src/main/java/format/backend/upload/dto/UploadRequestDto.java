cff4ec0aaf21d6a12de0d0a793bb7af94d428b9c
Paw565pl <paw565pl@gmail.com>
2025-12-18 15:03:36 +0100

    feat: create image extension validator

--- backend/src/main/java/format/backend/upload/dto/UploadRequestDto.java
+++ backend/src/main/java/format/backend/upload/dto/UploadRequestDto.java
@@ -1,8 +1,10 @@
 package format.backend.upload.dto;
 
+import format.backend.upload.validator.ValidImageExtension;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.jspecify.annotations.NonNull;
 
 public record UploadRequestDto(
+        @ValidImageExtension
         @Size(min = 3, max = 200, message = "FileName must be between 3 and 200 characters long") @NotBlank(message = "FileName cannot be blank") @NonNull String fileName) {}
