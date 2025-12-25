931e09c99d6eb83a3991312d987d96b866742367
Mateusz Domino <mdomino60@gmail.com>
2025-12-25 21:56:25 +0100

    FEAT: add methods to rating repo

--- backend/src/main/java/format/backend/commentRating/repository/CommentRatingRepository.java
+++ backend/src/main/java/format/backend/commentRating/repository/CommentRatingRepository.java
@@ -3,4 +3,16 @@ package format.backend.commentRating.repository;
 import format.backend.commentRating.entity.CommentRatingEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CommentRatingRepository extends MongoRepository<CommentRatingEntity, String> {}
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRatingRepository extends MongoRepository<CommentRatingEntity, String> {
+    Optional<CommentRatingEntity> findByCommentIdAndAuthorId(String commentId, String authorId);
+
+    List<CommentRatingEntity> findAllByCommentIdIn(Collection<String> commentIds);
+
+    void deleteAllByCommentId(String commentId);
+
+    void deleteAllByCommentIdIn(Collection<String> commentIds);
+}
