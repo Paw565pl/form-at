b1db845e9bbc4755a0c9b6a867d4e9a25f6c9e48
Mateusz Domino <mdomino60@gmail.com>
2025-12-26 02:30:00 +0100

    REFACTOR: remove unnecesary toEntity mapper method

--- backend/src/main/java/format/backend/commentRating/mapper/CommentRatingMapper.java
+++ backend/src/main/java/format/backend/commentRating/mapper/CommentRatingMapper.java
@@ -1,7 +1,5 @@
 package format.backend.commentRating.mapper;
 
-import format.backend.comment.entity.CommentEntity;
-import format.backend.commentRating.dto.CommentRatingRequestDto;
 import format.backend.commentRating.dto.CommentRatingResponseDto;
 import format.backend.commentRating.entity.CommentRatingEntity;
 import org.mapstruct.Mapper;
@@ -14,13 +12,4 @@ public interface CommentRatingMapper {
     @Mapping(target = "commentId", source = "commentRating.comment.id")
     @Mapping(target = "type", expression = "java(RatingType.fromValue(commentRating.getType()))")
     CommentRatingResponseDto toResponseDto(CommentRatingEntity commentRating);
-
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "comment", source = "comment")
-    @Mapping(target = "type", expression = "java(commentRequestDto.type().getValue())")
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "version", ignore = true)
-    CommentRatingEntity toEntity(CommentRatingRequestDto commentRequestDto, CommentEntity comment);
 }
