1059c0de7150b514d99716b1398c2e090463c31a
Mateusz Domino <mdomino60@gmail.com>
2025-12-26 02:23:31 +0100

    REFACTOR: remove unnecesary repo method

--- backend/src/main/java/format/backend/commentRating/repository/CommentRatingRepository.java
+++ backend/src/main/java/format/backend/commentRating/repository/CommentRatingRepository.java
@@ -4,13 +4,10 @@ import format.backend.commentRating.entity.CommentRatingEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 public interface CommentRatingRepository extends MongoRepository<CommentRatingEntity, String> {
     Optional<CommentRatingEntity> findByCommentIdAndAuthorId(String commentId, String authorId);
-
-    List<CommentRatingEntity> findAllByCommentIdIn(Collection<String> commentIds);
 
     void deleteAllByCommentId(String commentId);
 
