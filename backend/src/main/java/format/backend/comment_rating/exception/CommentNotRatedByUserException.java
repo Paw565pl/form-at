3278623d75e14f97f96ecab26b308d685308748a
Mateusz Domino <mdomino60@gmail.com>
2025-12-29 20:02:53 +0100

    FIX: add final to CommentNotRatedByUserException class

--- backend/src/main/java/format/backend/comment_rating/exception/CommentNotRatedByUserException.java
+++ backend/src/main/java/format/backend/comment_rating/exception/CommentNotRatedByUserException.java
@@ -4,7 +4,7 @@ import format.backend.core.exception.ApplicationException;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
 
-public class CommentNotRatedByUserException extends ApplicationException {
+public final class CommentNotRatedByUserException extends ApplicationException {
     public CommentNotRatedByUserException(@NonNull String commentId) {
         super(HttpStatus.NOT_FOUND, String.format("Comment wih id '%s' has not been rated by the user", commentId));
     }
