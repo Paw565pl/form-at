1f6bd8015d72cca351283abefcc099a7fc02f206
Mateusz Domino <mdomino60@gmail.com>
2025-12-30 21:58:13 +0100

    FEAT: add NotNull adnotation to CommentRatingRequestDto

--- backend/src/main/java/format/backend/comment_rating/dto/CommentRatingRequestDto.java
+++ backend/src/main/java/format/backend/comment_rating/dto/CommentRatingRequestDto.java
@@ -1,6 +1,7 @@
 package format.backend.comment_rating.dto;
 
 import format.backend.comment_rating.entity.RatingType;
+import jakarta.validation.constraints.NotNull;
 import org.jspecify.annotations.NonNull;
 
-public record CommentRatingRequestDto(@NonNull RatingType type) {}
+public record CommentRatingRequestDto(@NotNull @NonNull RatingType type) {}
