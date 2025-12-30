8833ca2ffa1418886c7c5f792c8eea12d9542ba9
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2025-12-30 22:19:55 +0100

    fix: minor layout changes

--- frontend/src/features/user-profile/components/user-profile.tsx
+++ frontend/src/features/user-profile/components/user-profile.tsx
@@ -25,38 +25,37 @@ export const UserProfile = ({ username }: UserProfileProps) => {
   return (
     <section
       id="user-profile"
-      className="flex w-full flex-col gap-2 px-5 py-10 lg:px-30"
+      className="flex w-full flex-col gap-4 px-5 py-10 lg:px-30"
     >
       <div className="bg-card rounded-lg border p-6">
         {/* Profile Header */}
-        <div className="flex items-start gap-4 pb-6">
+        <div className="flex flex-col items-center gap-4 md:flex-row md:items-start">
           <UserImage className="h-48 w-48" />
-          <h1 className="pt-8 text-2xl font-bold md:text-3xl">
+          <h1 className="text-2xl font-bold md:pt-6 md:text-3xl">
             {userProfile.username}
           </h1>
         </div>
-
-        {/* Statistics */}
-        <div className="flex flex-col gap-4 sm:flex-row">
-          <Card className="flex flex-1 flex-col items-center rounded-md border p-4">
-            <CardTitle>{userProfile.statistics.formsCount}</CardTitle>
-            <CardDescription className="text-center">
-              {t("formsCount")}
-            </CardDescription>
-          </Card>
-          <Card className="flex flex-1 flex-col items-center rounded-md border p-4">
-            <CardTitle>{userProfile.statistics.submissionsCount}</CardTitle>
-            <CardDescription className="text-center">
-              {t("submissionsCount")}
-            </CardDescription>
-          </Card>
-          <Card className="flex flex-1 flex-col items-center rounded-md border p-4">
-            <CardTitle>{userProfile.statistics.commentsCount}</CardTitle>
-            <CardDescription className="text-center">
-              {t("commentsCount")}
-            </CardDescription>
-          </Card>
-        </div>
+      </div>
+      {/* Statistics */}
+      <div className="flex flex-col gap-4 sm:flex-row">
+        <Card className="flex flex-1 flex-col items-center rounded-md border p-4">
+          <CardTitle>{userProfile.statistics.formsCount}</CardTitle>
+          <CardDescription className="text-center">
+            {t("formsCount")}
+          </CardDescription>
+        </Card>
+        <Card className="flex flex-1 flex-col items-center rounded-md border p-4">
+          <CardTitle>{userProfile.statistics.submissionsCount}</CardTitle>
+          <CardDescription className="text-center">
+            {t("submissionsCount")}
+          </CardDescription>
+        </Card>
+        <Card className="flex flex-1 flex-col items-center rounded-md border p-4">
+          <CardTitle>{userProfile.statistics.commentsCount}</CardTitle>
+          <CardDescription className="text-center">
+            {t("commentsCount")}
+          </CardDescription>
+        </Card>
       </div>
     </section>
   );
