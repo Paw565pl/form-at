e5699ec31ca7b23a4fe86d037d5fd5353b97001d
Mateusz Domino <mdomino60@gmail.com>
2025-12-31 18:17:53 +0100

    FIX: fix date and id conversion

--- backend/src/main/java/format/backend/comment/converter/CommentResponseDtoConverter.java
+++ backend/src/main/java/format/backend/comment/converter/CommentResponseDtoConverter.java
@@ -3,7 +3,9 @@ package format.backend.comment.converter;
 import format.backend.comment.dto.CommentResponseDto;
 import format.backend.comment_rating.entity.RatingType;
 import java.time.Instant;
+import java.util.Date;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.jspecify.annotations.NonNull;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
@@ -14,13 +16,23 @@ public class CommentResponseDtoConverter implements Converter<@NonNull Document,
     public CommentResponseDto convert(Document source) {
         Integer userRatingRaw = source.getInteger("userRating");
 
+        ObjectId id = source.getObjectId("_id");
+        if (id == null) {
+            throw new IllegalStateException("CommentId cannot be null");
+        }
+
+        String idString = id.toHexString();
+
+        Date createdAtDate = source.getDate("createdAt");
+        Date updatedAtDate = source.getDate("updatedAt");
+
         return new CommentResponseDto(
-                source.getString("_id"),
+                idString,
                 source.getString("authorName"),
                 source.getString("content"),
                 source.getLong("ratingScore"),
                 userRatingRaw != null ? RatingType.fromValue(userRatingRaw).orElse(null) : null,
-                source.get("createdAt", Instant.class),
-                source.get("updatedAt", Instant.class));
+                createdAtDate.toInstant(),
+                updatedAtDate.toInstant());
     }
 }
