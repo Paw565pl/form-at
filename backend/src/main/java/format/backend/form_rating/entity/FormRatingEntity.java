f045dfd02f585a327e9e3c4003ea1a14d3437033
Mateusz Domino <mdomino60@gmail.com>
2025-12-31 19:15:30 +0100

    FEAT: wip

--- backend/src/main/java/format/backend/form_rating/entity/FormRatingEntity.java
+++ backend/src/main/java/format/backend/form_rating/entity/FormRatingEntity.java
@@ -1,4 +1,21 @@
 package format.backend.form_rating.entity;
 
+
+import format.backend.form.entity.FormEntity;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document(collection = "form_ratings")
 public class FormRatingEntity {
+
+    @MongoId
+    @Field(name = "_id", targetType = FieldType.OBJECT_ID)
+    private String id;
+
+    @DocumentReference(lazy = true)
+    @Field(name = "commentId")
+    @NonNull private FormEntity comment;
 }
--- backend/src/main/java/format/backend/form_rating/repository/FormRatingRepository.java
+++ backend/src/main/java/format/backend/form_rating/repository/FormRatingRepository.java
@@ -1,4 +1,9 @@
 package format.backend.form_rating.repository;
 
-public interface FormRatingRepository {
+import format.backend.form_rating.entity.FormRatingEntity;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface FormRatingRepository extends MongoRepository<@NonNull FormRatingEntity, @NonNull String> {
+
 }
