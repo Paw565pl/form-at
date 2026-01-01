b2c563968f2602c4862e88a0b581a21e9cfadab7
Mateusz Domino <mdomino60@gmail.com>
2026-01-01 22:37:12 +0100

    feat: add formrating controller

--- backend/src/main/java/format/backend/form_rating/controller/FormRatingController.java
+++ backend/src/main/java/format/backend/form_rating/controller/FormRatingController.java
@@ -1,3 +1,44 @@
 package format.backend.form_rating.controller;
 
-public class FormRatingController {}
+import format.backend.auth.annotation.IsAuthenticated;
+import format.backend.auth.jwt.KeycloakJwtClaimsExtractor;
+import format.backend.comment_rating.dto.CommentRatingResponseDto;
+import format.backend.form_rating.dto.FormRatingRequestDto;
+import format.backend.form_rating.service.FormRatingService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/forms/{formIdOrSlug}/rating")
+public class FormRatingController {
+
+    private final KeycloakJwtClaimsExtractor keycloakJwtClaimsExtractor;
+    private final FormRatingService formRatingService;
+
+    @IsAuthenticated
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentRatingResponseDto add(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable String formIdOrSlug,
+            @Valid @RequestBody FormRatingRequestDto formRatingRequestDto) {
+        return formRatingService.add(
+                formIdOrSlug, keycloakJwtClaimsExtractor.getClaims(jwt), formRatingRequestDto);
+    }
+
+    @IsAuthenticated
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable String formIdOrSlug) {
+        formRatingService.delete(formIdOrSlug, keycloakJwtClaimsExtractor.getClaims(jwt));
+    }
+}
