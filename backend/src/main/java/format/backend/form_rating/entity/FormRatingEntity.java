ba683bce5ee103365a2e035ee519b1c1b8d36a88
Mateusz Domino <mdomino60@gmail.com>
2026-01-01 19:01:01 +0100

    FEAT: finish formrating entity

--- backend/src/main/java/format/backend/form_rating/entity/FormRatingEntity.java
+++ backend/src/main/java/format/backend/form_rating/entity/FormRatingEntity.java
@@ -1,13 +1,32 @@
 package format.backend.form_rating.entity;
 
 
+import format.backend.auth.entity.UserEntity;
 import format.backend.form.entity.FormEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import java.time.Instant;
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@CompoundIndex(name = "idx_formId_authorId_unique", def = "{'formId': 1, 'authorId': 1}", unique = true)
 @Document(collection = "form_ratings")
 public class FormRatingEntity {
 
@@ -16,6 +35,27 @@ public class FormRatingEntity {
     private String id;
 
     @DocumentReference(lazy = true)
-    @Field(name = "commentId")
+    @Field(name = "formId")
     @NonNull private FormEntity comment;
+
+    @DocumentReference(lazy = true)
+    @Field(name = "authorId")
+    @NonNull
+    private UserEntity author;
+
+    @Field(name = "rating", targetType = FieldType.INT32)
+    private Integer rating;
+
+    @CreatedDate
+    @Field("createdAt")
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Indexed(direction = IndexDirection.DESCENDING)
+    @Field("updatedAt")
+    private Instant updatedAt;
+
+    @Version
+    @Field(name = "version")
+    private Long version;
 }
