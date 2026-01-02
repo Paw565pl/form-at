a03e41238fd9b6493a7dbed750d7a1e0b87c11ee
Mateusz Domino <mdomino60@gmail.com>
2026-01-02 22:01:12 +0100

    fix: formlistaggregationresult type for ratingcount from integer to long to match enitity

--- backend/src/main/java/format/backend/form/entity/FormListAggregationResult.java
+++ backend/src/main/java/format/backend/form/entity/FormListAggregationResult.java
@@ -32,7 +32,7 @@ public record FormListAggregationResult(
 
         @NonNull Long submissionsCount,
 
-        @NonNull Integer questionsCount,
+        @NonNull Long questionsCount,
 
         @NonNull Long ratingCount,
 
