bee48cf49adbd287182474b688a5cda51ea137fc
Mateusz Domino <mdomino60@gmail.com>
2026-01-05 17:05:20 +0100

    refactor: remove unnecessary validated annotation

--- backend/src/main/java/format/backend/form_rating/controller/FormRatingController.java
+++ backend/src/main/java/format/backend/form_rating/controller/FormRatingController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/forms/{formIdOrSlug}/rating")
