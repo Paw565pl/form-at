517d340ec388b6a5be3dd24f1a304a57693b89f0
Paw565pl <paw565pl@gmail.com>
2026-01-14 12:41:25 +0100

    chore: add server page

---
+++ frontend/src/features/form-create/components/form-create-form.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { FormRequest } from "@/core/types/form";
+import { useCreateForm } from "@/features/form-create/hooks/use-create-form";
+import { FormBaseForm } from "@/features/form/components/form-base-form";
+import { AxiosError } from "axios";
+import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export const FormCreateForm = () => {
+  const t = useTranslations("formCreatePage");
+
+  const router = useRouter();
+  const {
+    mutate: createForm,
+    isPending,
+    uploadProgressPercent,
+  } = useCreateForm();
+
+  const onSubmit = (request: FormRequest) => {
+    createForm(request, {
+      onSuccess: (data) => {
+        toast.success(t("successMessage"));
+        router.push(`/forms/${data.slug}`);
+      },
+      onError: (error) => {
+        if (error instanceof AxiosError) {
+          toast.error(error.response?.data.message);
+        } else {
+          toast.error(t("errorMessage"));
+        }
+      },
+    });
+  };
+
+  return (
+    <FormBaseForm
+      pageTitle={t("pageTitle")}
+      isPending={isPending}
+      uploadProgressPercent={uploadProgressPercent}
+      onSubmit={onSubmit}
+    />
+  );
+};
--- frontend/src/features/form-create/form-create-page.tsx
+++ frontend/src/features/form-create/form-create-page.tsx
@@ -1,45 +1,5 @@
-"use client";
-
-import { FormRequest } from "@/core/types/form";
-import { useCreateForm } from "@/features/form-create/hooks/use-create-form";
-import { FormBaseForm } from "@/features/form/components/form-base-form";
-import { AxiosError } from "axios";
-import { useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
+import { FormCreateForm } from "@/features/form-create/components/form-create-form";
 
 export const FormCreatePage = () => {
-  const t = useTranslations("formCreatePage");
-
-  const router = useRouter();
-  const {
-    mutate: createForm,
-    isPending,
-    uploadProgressPercent,
-  } = useCreateForm();
-
-  const onSubmit = (request: FormRequest) => {
-    createForm(request, {
-      onSuccess: (data) => {
-        toast.success(t("successMessage"));
-        router.push(`/forms/${data.slug}`);
-      },
-      onError: (error) => {
-        if (error instanceof AxiosError) {
-          toast.error(error.response?.data.message);
-        } else {
-          toast.error(t("errorMessage"));
-        }
-      },
-    });
-  };
-
-  return (
-    <FormBaseForm
-      pageTitle={t("pageTitle")}
-      isPending={isPending}
-      uploadProgressPercent={uploadProgressPercent}
-      onSubmit={onSubmit}
-    />
-  );
+  return <FormCreateForm />;
 };
