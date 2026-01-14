b5cb7ace74a00c8c021a8143ce212259687e62fe
Paw565pl <paw565pl@gmail.com>
2026-01-14 12:39:25 +0100

    feat: add form to form edit page

--- frontend/src/features/form-edit/components/form-edit-form.tsx
+++ frontend/src/features/form-edit/components/form-edit-form.tsx
@@ -1,3 +1,58 @@
-export const FormEditForm = () => {
-  return <div>FormEditForm</div>;
+"use client";
+
+import { FormRequest } from "@/core/types/form";
+import { useFetchFormDetails } from "@/features/form-details/hooks/use-fetch-form-details";
+import { useUpdateForm } from "@/features/form-edit/hooks/use-update.form";
+import { FormBaseForm } from "@/features/form/components/form-base-form";
+import { AxiosError, HttpStatusCode } from "axios";
+import { useTranslations } from "next-intl";
+import { notFound, useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+interface FormEditFormProps {
+  readonly slug: string;
+}
+
+export const FormEditForm = ({ slug }: FormEditFormProps) => {
+  const t = useTranslations("formEditPage");
+
+  const router = useRouter();
+  const { data: formDetails, isLoading, error } = useFetchFormDetails(slug);
+  const {
+    mutate: updateForm,
+    isPending,
+    uploadProgressPercent,
+  } = useUpdateForm(slug);
+
+  if (error) {
+    if (error.status === HttpStatusCode.NotFound) return notFound();
+    else throw error;
+  }
+
+  if (isLoading || !formDetails) return <p>Loading...</p>; // TODO: add translation
+
+  const onSubmit = (request: FormRequest) => {
+    updateForm(request, {
+      onSuccess: (data) => {
+        toast.success(t("successMessage"));
+        router.push(`/forms/${data.slug}`);
+      },
+      onError: (error) => {
+        if (error instanceof AxiosError) {
+          toast.error(error.response?.data.message);
+        } else {
+          toast.error(t("errorMessage"));
+        }
+      },
+    });
+  };
+
+  return (
+    <FormBaseForm
+      pageTitle={t("pageTitle")}
+      isPending={isPending}
+      uploadProgressPercent={uploadProgressPercent}
+      onSubmit={onSubmit}
+    />
+  );
 };
