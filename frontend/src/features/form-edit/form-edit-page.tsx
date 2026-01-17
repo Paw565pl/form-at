d4fc9e6460ee057c35800e62163030ca850f7a2d
Paw565pl <paw565pl@gmail.com>
2026-01-17 12:17:28 +0100

    feat: add role check to form edit page

--- frontend/src/features/form-edit/form-edit-page.tsx
+++ frontend/src/features/form-edit/form-edit-page.tsx
@@ -1,15 +1,30 @@
 import { getQueryClient } from "@/core/lib/tanstack-query";
-import { prefetchFormDetails } from "@/features/form-details/hooks/use-fetch-form-details";
+import { auth } from "@/features/auth/config/auth-config";
+import { Role } from "@/features/auth/types/role";
+import {
+  getFetchFormDetailsQueryOptions,
+  prefetchFormDetails,
+} from "@/features/form-details/hooks/use-fetch-form-details";
 import { FormEditForm } from "@/features/form-edit/components/form-edit-form";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { notFound } from "next/navigation";
 
 export const FormEditPage = async ({
   params,
 }: PageProps<"/forms/[slug]/edit">) => {
+  const session = await auth();
   const { slug } = await params;
 
   const queryClient = getQueryClient();
   await prefetchFormDetails(queryClient, slug);
+
+  const formDetails = queryClient.getQueryData(
+    getFetchFormDetailsQueryOptions(slug).queryKey,
+  );
+  const isFormAuthor = session?.user.name === formDetails?.authorName;
+  const isAdmin = session?.user.roles.includes(Role.ADMIN);
+
+  if (!(isFormAuthor || isAdmin)) return notFound();
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
