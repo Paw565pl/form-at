59dd7b71312d7f88a9f45ef033ef0f4e48ec1915
Paw565pl <paw565pl@gmail.com>
2026-01-20 15:35:38 +0100

    chore: extract record

--- backend/src/main/java/format/backend/submission/dto/SubmissionStatisticsResponseDto.java
+++ backend/src/main/java/format/backend/submission/dto/SubmissionStatisticsResponseDto.java
@@ -3,6 +3,4 @@ package format.backend.submission.dto;
 import java.util.List;
 
 public record SubmissionStatisticsResponseDto(
-        String questionId, List<AnswersStatisticsResponseDto> submissionStatistics) {}
-
-record AnswersStatisticsResponseDto(String answerId, Integer totalCount) {}
+        String questionId, List<SubmissionAnswersStatisticsResponseDto> submissionStatistics) {}
