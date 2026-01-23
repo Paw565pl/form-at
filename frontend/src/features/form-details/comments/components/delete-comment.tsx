130c60f9012f729cbccec09912fe8b71a17bcbfa
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-01-23 15:11:00 +0100

    feat: refactor comment deletion functionality by replacing DeleteCommentAlertDialog with DeleteComment component

---
+++ frontend/src/features/form-details/comments/components/delete-comment.tsx
@@ -0,0 +1,74 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/core/components/ui/alert-dialog";
+import { DropdownMenuItem } from "@/core/components/ui/dropdown-menu";
+import { ICONS } from "@/core/config/icons";
+import { useTranslations } from "next-intl";
+import { toast } from "sonner";
+import { useDeleteComment } from "../hooks/use-delete-comment";
+
+interface DeleteCommentAlertDialogProps {
+  readonly formIdOrSlug: string;
+  readonly commentId: string;
+}
+
+export const DeleteComment = ({
+  formIdOrSlug,
+  commentId,
+}: DeleteCommentAlertDialogProps) => {
+  const t = useTranslations("formDetailsPage.comments");
+
+  const { mutate: deleteComment, isPending } = useDeleteComment(
+    formIdOrSlug,
+    commentId,
+  );
+
+  const handleDelete = () =>
+    deleteComment(undefined, {
+      onSuccess: () => {
+        toast.success(t("deleteSuccess"));
+      },
+      onError: () => {
+        toast.error(t("deleteError"));
+      },
+    });
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <DropdownMenuItem
+          disabled={isPending}
+          variant="destructive"
+          onSelect={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <ICONS.delete />
+          {t("delete")}
+        </DropdownMenuItem>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{t("deleteDialogTitle")}</AlertDialogTitle>
+          <AlertDialogDescription>
+            {t("deleteDialogDescription")}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>{t("cancel")}</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete} disabled={isPending}>
+            {t("delete")}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
