3228115a464041f37ad9bb1f339ce44620ec7963
oworobjew <oworobjew@gmail.com>
2026-01-23 12:25:03 +0100

    fix: allow only admin or owner access to pages

--- frontend/src/features/submission-details/submission-details-page.tsx
+++ frontend/src/features/submission-details/submission-details-page.tsx
@@ -1,12 +1,19 @@
 import { getQueryClient } from "@/core/lib/tanstack-query";
-import { prefetchFormDetails } from "@/features/form-details/hooks/use-fetch-form-details";
+import { auth } from "@/features/auth/config/auth-config";
+import { Role } from "@/features/auth/types/role";
+import {
+  getFetchFormDetailsQueryOptions,
+  prefetchFormDetails,
+} from "@/features/form-details/hooks/use-fetch-form-details";
 import { SubmissionDetails } from "@/features/submission-details/components/submission-details";
 import { prefetchSubmissionDetails } from "@/features/submission-details/hooks/use-fetch-submission-details";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { notFound } from "next/navigation";
 
 export const SubmissionDetailsPage = async ({
   params,
 }: PageProps<"/forms/[slug]/submissions/[submissionId]">) => {
+  const session = await auth();
   const { slug, submissionId } = await params;
 
   const queryClient = getQueryClient();
@@ -14,6 +21,16 @@ export const SubmissionDetailsPage = async ({
     prefetchFormDetails(queryClient, slug),
     prefetchSubmissionDetails(queryClient, slug, submissionId),
   ]);
+
+  const formDetails = queryClient.getQueryData(
+    getFetchFormDetailsQueryOptions(slug).queryKey,
+  );
+
+  const isFormAuthor = session?.user.name === formDetails?.authorName;
+  const isAdmin = session?.user.roles.includes(Role.ADMIN);
+
+  if (!(isFormAuthor || isAdmin || formDetails?.saveSubmissions))
+    return notFound();
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
--- frontend/src/features/submission-list/submission-list-page.tsx
+++ frontend/src/features/submission-list/submission-list-page.tsx
@@ -1,12 +1,19 @@
 import { getQueryClient } from "@/core/lib/tanstack-query";
-import { prefetchFormDetails } from "@/features/form-details/hooks/use-fetch-form-details";
+import { auth } from "@/features/auth/config/auth-config";
+import { Role } from "@/features/auth/types/role";
+import {
+  getFetchFormDetailsQueryOptions,
+  prefetchFormDetails,
+} from "@/features/form-details/hooks/use-fetch-form-details";
 import { Submissions } from "@/features/submission-list/components/submissions";
 import { prefetchSubmissionPages } from "@/features/submission-list/hooks/use-fetch-submission-pages";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { notFound } from "next/navigation";
 
 export const SubmissionListPage = async ({
   params,
 }: PageProps<"/forms/[slug]/submissions">) => {
+  const session = await auth();
   const { slug } = await params;
 
   const queryClient = getQueryClient();
@@ -14,6 +21,16 @@ export const SubmissionListPage = async ({
     prefetchSubmissionPages(queryClient, slug),
     prefetchFormDetails(queryClient, slug),
   ]);
+
+  const formDetails = queryClient.getQueryData(
+    getFetchFormDetailsQueryOptions(slug).queryKey,
+  );
+
+  const isFormAuthor = session?.user.name === formDetails?.authorName;
+  const isAdmin = session?.user.roles.includes(Role.ADMIN);
+
+  if (!(isFormAuthor || isAdmin || formDetails?.saveSubmissions))
+    return notFound();
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
