d516465d3d58ec3275e9cd5d32ecc0c27663156b
oworobjew <oworobjew@gmail.com>
2026-02-28 12:55:44 +0100

    fix: destructive dialogs, comment wrappis, submission dialog translation improvements

--- frontend/src/features/form-details/comments/comments.tsx
+++ frontend/src/features/form-details/comments/comments.tsx
@@ -92,7 +92,9 @@ export const Comments = ({ formIdOrSlug }: CommentsProps) => {
                 />
               ) : (
                 <>
-                  <p className="py-2 text-sm">{comment.content}</p>
+                  <p className="py-2 text-sm text-pretty wrap-break-word">
+                    {comment.content}
+                  </p>
 
                   <RatingButtons
                     ratingScore={comment.ratingScore}
