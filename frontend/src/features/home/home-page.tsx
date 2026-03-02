31a328e209c9e5631f6bb96f70d6c565dc9b727e
oworobjew <oworobjew@gmail.com>
2026-03-02 12:26:10 +0100

    fix: lint

--- frontend/src/features/home/home-page.tsx
+++ frontend/src/features/home/home-page.tsx
@@ -1,7 +1,7 @@
 import { getQueryClient } from "@/core/lib/tanstack-query";
 import { Home } from "@/features/home/components/home";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { prefetchStatistics } from "@/features/home/hooks/use-fetch-statistics";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 export const HomePage = async () => {
   const queryClient = getQueryClient();
