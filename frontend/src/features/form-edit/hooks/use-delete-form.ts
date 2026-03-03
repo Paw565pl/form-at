8d7623beb6ba1d697888d8184831357edfd2a3ba
Paw565pl <paw565pl@gmail.com>
2026-03-03 12:58:08 +0100

    refactor: simplify func

--- frontend/src/features/form-edit/hooks/use-delete-form.ts
+++ frontend/src/features/form-edit/hooks/use-delete-form.ts
@@ -7,10 +7,7 @@ export const useDeleteForm = (idOrSlug: string) =>
   useMutation<undefined, AxiosError<ErrorResponseDto>, undefined>({
     mutationKey: ["forms", idOrSlug, "delete"],
     mutationFn: async () => {
-      const { data } = await apiService.delete<undefined>(
-        `/api/v1/forms/${idOrSlug}`,
-      );
-      return data;
+      await apiService.delete<undefined>(`/api/v1/forms/${idOrSlug}`);
     },
     onSuccess: (_, __, ___, { client }) => {
       client.invalidateQueries({
