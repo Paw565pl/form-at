36ef6d1d3b2011dd0b976762c839438410273b39
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-03-04 18:20:52 +0100

    feat: add custom fonts and apply them to global styles

--- frontend/src/core/layouts/root-layout.tsx
+++ frontend/src/core/layouts/root-layout.tsx
@@ -4,13 +4,36 @@ import { ClientProviders } from "@/core/providers/client-providers";
 import { ServerProviders } from "@/core/providers/server-providers";
 import { getLocale } from "next-intl/server";
 import { PublicEnvScript } from "next-runtime-env";
+import { Cascadia_Mono, Montserrat, Noto_Serif } from "next/font/google";
 import { PropsWithChildren } from "react";
+
+const noto_serif = Noto_Serif({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  display: "swap",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+});
+
+const cascadia_mono = Cascadia_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+  display: "swap",
+});
 
 export const RootLayout = async ({ children }: PropsWithChildren) => {
   const locale = await getLocale();
 
   return (
-    <html lang={locale} suppressHydrationWarning>
+    <html
+      lang={locale}
+      className={`${noto_serif.variable} ${montserrat.variable} ${cascadia_mono.variable} antialiased`}
+      suppressHydrationWarning
+    >
       {/* eslint-disable-next-line @next/next/no-head-element */}
       <head>
         <PublicEnvScript />
