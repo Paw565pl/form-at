98499eec99fbdbc21b6d05de718ee9ae6fafe0a4
Paw565pl <paw565pl@gmail.com>
2026-03-23 13:18:19 +0100

    chore: remove needless values

--- backend/src/main/java/format/backend/upload/validator/ImageExtension.java
+++ backend/src/main/java/format/backend/upload/validator/ImageExtension.java
@@ -1,10 +1,7 @@
 package format.backend.upload.validator;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
@@ -12,19 +9,16 @@ import lombok.val;
 @Getter
 @RequiredArgsConstructor
 public enum ImageExtension {
-    PNG("png"),
-    JPG("jpg"),
-    JPEG("jpeg"),
-    WEBP("webp"),
     AVIF("avif");
 
     private final String value;
 
-    private static final Map<String, ImageExtension> valueToImageExtensionMap =
-            Arrays.stream(values()).collect(Collectors.toUnmodifiableMap(i -> i.value, Function.identity()));
-
     public static Optional<ImageExtension> from(String extension) {
-        return Optional.ofNullable(valueToImageExtensionMap.get(extension.toLowerCase()));
+        if (!Objects.equals(extension.toLowerCase(), AVIF.value)) {
+            return Optional.empty();
+        }
+
+        return Optional.of(AVIF);
     }
 
     public static Optional<ImageExtension> fromFilename(String filename) {
