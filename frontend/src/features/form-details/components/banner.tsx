a0ff17d3005a615f6498d5e8704174366b6586e6
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2026-04-03 12:50:11 +0200

    fix: hide view my submissions button when results are not being shown

--- frontend/src/features/form-details/components/banner.tsx
+++ frontend/src/features/form-details/components/banner.tsx
@@ -72,19 +72,21 @@ export const Banner = ({ form }: BannerProps) => {
       )}
 
       <div className="absolute right-2 bottom-2 flex items-center gap-2 md:right-4 md:bottom-4">
-        {!mySubmission ? (
-          <Button
-            onClick={() => router.push(`/forms/${form.slug}/submissions/new`)}
-          >
-            <ICONS.fillSubmission />
-            {t("fillOutForm")}
-          </Button>
-        ) : (
+        {mySubmission && form.showAnswersFeedback && (
           <Button
             onClick={() => router.push(`/forms/${form.slug}/submissions/new`)}
           >
             <ICONS.submissions />
             {t("viewMySubmission")}
+          </Button>
+        )}
+
+        {!mySubmission && (
+          <Button
+            onClick={() => router.push(`/forms/${form.slug}/submissions/new`)}
+          >
+            <ICONS.fillSubmission />
+            {t("fillOutForm")}
           </Button>
         )}
 
