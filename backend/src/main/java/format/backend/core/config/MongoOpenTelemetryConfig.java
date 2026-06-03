78369936b94d436df812e099397d63660fface43
Paw565pl <paw565pl@gmail.com>
2026-06-03 13:32:23 +0200

    feat: create mongo metrics bean

---
+++ backend/src/main/java/format/backend/core/config/MongoOpenTelemetryConfig.java
@@ -0,0 +1,19 @@
+package format.backend.core.config;
+
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.boot.mongodb.autoconfigure.MongoClientSettingsBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.observability.ContextProviderFactory;
+import org.springframework.data.mongodb.observability.MongoObservationCommandListener;
+
+@Configuration
+class MongoOpenTelemetryConfig {
+
+    @Bean
+    MongoClientSettingsBuilderCustomizer mongoMetricsSynchronousContextProvider(ObservationRegistry registry) {
+        return (clientSettingsBuilder) -> clientSettingsBuilder
+                .contextProvider(ContextProviderFactory.create(registry))
+                .addCommandListener(new MongoObservationCommandListener(registry));
+    }
+}
