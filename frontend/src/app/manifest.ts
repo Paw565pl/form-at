9632e0d7e7109ba98982bca246b1f969a89e527d
Paw565pl <134140848+Paw565pl@users.noreply.github.com>
2026-06-29 19:45:46 +0200

    FIX: add missing fields to webmanifest (#136)

--- frontend/src/app/manifest.ts
+++ frontend/src/app/manifest.ts
@@ -8,7 +8,21 @@ const manifest = async (): Promise<MetadataRoute.Manifest> => {
     name: t("name"),
     short_name: t("name"),
     description: t("description"),
+
+    id: "/",
+    start_url: "/",
+    scope: "/",
+
+    display: "standalone",
+    theme_color: "#1F7771",
+    background_color: "#121212",
     icons: [
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
       {
         src: "/web-app-manifest-192x192.png",
         sizes: "192x192",
@@ -19,12 +33,15 @@ const manifest = async (): Promise<MetadataRoute.Manifest> => {
         src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
         purpose: "maskable",
       },
     ],
-    theme_color: "#1F7771",
-    background_color: "#121212",
-    display: "standalone",
   };
 };
 
