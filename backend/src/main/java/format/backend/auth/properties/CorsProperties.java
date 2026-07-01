1737d3b372e35adfd1aa34975db5ef3f0075b4ca
Paw565pl <paw565pl@gmail.com>
2026-07-01 11:50:27 +0200

    refactor: use records for properties

--- backend/src/main/java/format/backend/auth/properties/CorsProperties.java
+++ backend/src/main/java/format/backend/auth/properties/CorsProperties.java
@@ -1,24 +1,16 @@
 package format.backend.auth.properties;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
-@Getter
-@Setter
-@ToString
 @Validated
 @ConfigurationProperties("cors")
-public class CorsProperties {
+public record CorsProperties(
+        @NotEmpty List<@NotBlank String> allowedHeaders,
 
-    @NotEmpty private List<String> allowedHeaders = new ArrayList<>();
+        @NotEmpty List<@NotBlank String> allowedMethods,
 
-    @NotEmpty private List<String> allowedMethods = new ArrayList<>();
-
-    @NotEmpty private List<String> allowedOrigins = new ArrayList<>();
-}
+        @NotEmpty List<@NotBlank String> allowedOrigins) {}
