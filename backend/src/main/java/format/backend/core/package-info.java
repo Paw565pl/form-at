954bdea4769a2dd655d37809a48138a517ca8dc5
Paw565pl <paw565pl@gmail.com>
2026-08-11 10:31:32 +0200

    feat: create slugify config

---
+++ backend/src/main/java/format/backend/core/package-info.java
@@ -0,0 +1,6 @@
+@ApplicationModule(type = OPEN)
+package format.backend.core;
+
+import static org.springframework.modulith.ApplicationModule.Type.OPEN;
+
+import org.springframework.modulith.ApplicationModule;
