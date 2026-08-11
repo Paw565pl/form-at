2b63e64ff2eaa6a65409f1ca646b2d2a5a58c7dd
Paw565pl <paw565pl@gmail.com>
2026-08-11 10:31:47 +0200

    feat: create FailedEventsRetryJob

---
+++ backend/src/main/java/format/backend/core/job/FailedEventsRetryJob.java
@@ -0,0 +1,24 @@
+package format.backend.core.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.modulith.events.FailedEventPublications;
+import org.springframework.modulith.events.ResubmissionOptions;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+class FailedEventsRetryJob {
+
+    private final FailedEventPublications failedEventPublications;
+
+    @Scheduled(cron = "0 */1 * * * *")
+    void retry() {
+        log.debug("Retrying failed event publications.");
+        failedEventPublications.resubmit(ResubmissionOptions.defaults()
+                .withBatchSize(100)
+                .withFilter(eventPublication -> eventPublication.getCompletionAttempts() < 5));
+    }
+}
