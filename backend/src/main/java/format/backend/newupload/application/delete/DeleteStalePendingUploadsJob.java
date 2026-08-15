b16e6c0f0eb99b26b07dc71ecc1358717ce37a06
Paw565pl <paw565pl@gmail.com>
2026-08-15 17:25:12 +0200

    feat: implement DeleteStalePendingUploadsJob

---
+++ backend/src/main/java/format/backend/newupload/application/delete/DeleteStalePendingUploadsJob.java
@@ -0,0 +1,53 @@
+package format.backend.newupload.application.delete;
+
+import format.backend.newupload.domain.entity.UploadEntity;
+import format.backend.newupload.domain.entity.UploadStatus;
+import format.backend.newupload.domain.repository.UploadRepository;
+import format.backend.newupload.properties.UploadProperties;
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+class DeleteStalePendingUploadsJob {
+
+    private static final Pageable pageable = PageRequest.of(0, 500, Sort.by(Sort.Direction.ASC, UploadEntity::getId));
+
+    private final UploadProperties uploadProperties;
+    private final UploadRepository uploadRepository;
+    private final DeleteUploadsHandler deleteUploadsHandler;
+
+    @Scheduled(cron = "0 0 3 * * *")
+    void execute() {
+        log.debug("Deleting stale uploads.");
+        val stalenessWindow = Instant.now().plus(uploadProperties.expiration().pendingUploads());
+
+        var totalDeletedCount = 0L;
+        var totalDeleteFailedCount = 0L;
+        while (true) {
+            val stalePendingUploadKeys = uploadRepository
+                    .findAllByStatusAndCreatedAtAfter(UploadStatus.PENDING, stalenessWindow, pageable)
+                    .map(UploadEntity::getKey)
+                    .toSet();
+            if (stalePendingUploadKeys.isEmpty()) break;
+
+            val deletedCount = deleteUploadsHandler.handle(stalePendingUploadKeys);
+            totalDeletedCount += deletedCount;
+
+            if (stalePendingUploadKeys.size() != deletedCount) {
+                totalDeleteFailedCount += stalePendingUploadKeys.size() - deletedCount;
+            }
+        }
+
+        log.debug("Deleted {} stale uploads.", totalDeletedCount);
+        if (totalDeleteFailedCount > 0) log.warn("Failed to delete {} stale uploads.", totalDeleteFailedCount);
+    }
+}
