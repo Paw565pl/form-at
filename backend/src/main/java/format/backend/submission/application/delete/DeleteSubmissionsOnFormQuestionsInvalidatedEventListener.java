859e99a95418ff1544c490fcfb0146695df872e3
Paw565pl <paw565pl@gmail.com>
2026-08-19 10:27:25 +0200

    refactor: simplify name

--- backend/src/main/java/format/backend/submission/application/delete/DeleteSubmissionsOnFormQuestionsInvalidatedEventListener.java
+++ backend/src/main/java/format/backend/submission/application/delete/DeleteSubmissionsOnFormQuestionsInvalidatedEventListener.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-class DeleteSubmissionsAnswersOnFormQuestionsInvalidatedEventListener {
+class DeleteSubmissionsOnFormQuestionsInvalidatedEventListener {
 
     private final FormFacade formFacade;
 
