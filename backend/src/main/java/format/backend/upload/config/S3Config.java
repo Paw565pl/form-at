61a1c71193222c937ed29e0a3d6ab11b65761b5c
Paw565pl <paw565pl@gmail.com>
2026-08-19 15:41:30 +0200

    fix: lack of bucket init

--- backend/src/main/java/format/backend/upload/config/S3Config.java
+++ backend/src/main/java/format/backend/upload/config/S3Config.java
@@ -1,10 +1,16 @@
 package format.backend.upload.config;
 
 import format.backend.upload.properties.S3Properties;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration(proxyBeanMethods = false)
 class S3Config {
 
@@ -15,5 +21,18 @@ class S3Config {
                 .credentials(s3Properties.accessKey(), s3Properties.secretKey())
                 .region(s3Properties.region())
                 .build();
+    }
+
+    @Bean
+    ApplicationRunner initBucket(S3Properties s3Properties, MinioClient minioClient) {
+        return _ -> {
+            val bucketExists = minioClient.bucketExists(
+                    BucketExistsArgs.builder().bucket(s3Properties.bucket()).build());
+            if (!bucketExists) {
+                minioClient.makeBucket(
+                        MakeBucketArgs.builder().bucket(s3Properties.bucket()).build());
+                log.info("Bucket '{}' created.", s3Properties.bucket());
+            }
+        };
     }
 }
